b21074e7b018e087503cb8d61972f30ced30c023
seamane <seamane@users.noreply.github.com>
2014-11-24 16:16:58 -0700

    Update database.js

--- routes/database.js
+++ routes/database.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: '20nederland12',
+	password: 'default',
 	//database: 'clutchdb'
 	//database: 'clutch'
 });
