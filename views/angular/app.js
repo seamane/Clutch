a7390ee3782afd04466210f3a321d4ea986ac780
seamane <eric_ctr_seaman247@yahoo.com>
2014-12-01 15:47:02 -0700

    project buttons
    working on ng-repeat for project buttons in project.html

--- views/angular/app.js
+++ views/angular/app.js
@@ -49,13 +49,20 @@ app.controller('homeController',function($scope, $http,$cookieStore){//, myServi
 	$scope.getName = function(){
 		return $cookieStore.get('userInfo').fname;
 	}
+	$scope.getUsername = function(){
+		return $cookieStore.get('userInfo').username;
+	}
 
 	$scope.getProjects = function(){
 		$http.post('/getProjects',{
-			
+			'userid': $cookieStore.get('userInfo').id;
 		}).
 		success(function(data){
 
 		});
+	}
+
+	$scope.createProject = function(){
+
 	}
 });
